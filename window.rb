761af2e96566166e836504dbd0980c80cc64f534
selvagad <selvaggio.adam@gmail.com>
2018-02-07 14:15:26 +0100

    x

--- window.rb
+++ window.rb
@@ -3,26 +3,26 @@ require_relative 'map'
 require_relative 'player'
 require_relative 'tiles'
 require_relative 'enemy'
-require_relative 'potion'
-WIDTH, HEIGHT = 900, 480
+WIDTH, HEIGHT = 900, 900
 class Window < Gosu::Window
 
   # constructeur de la classe Window
   def initialize(width, height)
     super WIDTH, HEIGHT
 
+    self.caption= "BeSt GaMe EvEr"
+
     @background = Gosu::Image.new("image/sky.jpg", :tileable => true)
     #creation d'un array pour stocker les noms des maps et faire un choix de level random
     @maps = ["maps/test.txt", "maps/test1.txt"]
     @item = @maps[rand(@maps.length)]
-    @coeur = Gosu::Image.new("image/coeurplein.png")
-    @coeurvide = Gosu::Image.new("image/coeur.png")
+    @coeur = Gosu::Image.new("image/potion.png")
     @vie = 3
-    @map = Map.new("maps/test.txt")
+    @map = Map.new("maps/test1.txt")
     @enemy = Enemy.new(@map,"oiseau")
     @player = Player.new(@map, 400, 100)
 
-    @test = "p"
+
     @music = Gosu::Song.new("song/miami.mp3")
     @music.volume = 0.25
     @music.play(true)
@@ -32,15 +32,13 @@ class Window < Gosu::Window
   end
 
   def update
-    @rect1 =  [@player.x, @player.y, 40, 36]
-    @rect2 = [@enemy.x,@enemy.y,50, 40]
     move_x = 0
     move_x -= 5 if Gosu.button_down? Gosu::KB_LEFT
     move_x += 5 if Gosu.button_down? Gosu::KB_RIGHT
     #i = 1 if Gosu.button_down? Gosu::KB_DOWN
 
     @player.update(move_x)
-    @player.collect_potion(@map.potion)
+  #  @cptn.collect_gems(@map.gems)
     # Scrolling follows player
     @camera_x = [[@player.x - WIDTH / 2, 0].max, @map.width * 50 - WIDTH].min
     @camera_y = [[@player.y - HEIGHT / 2, 0].max, @map.height * 50 - HEIGHT].min
@@ -49,11 +47,14 @@ class Window < Gosu::Window
 
   def draw
     @background.draw 0, 0, 0
+    @rect1 =  [@player.x, @player.y, 40, 36]
+    @rect2 = [@enemy.x,@enemy.y,36, 30]
+
     if (@rect1[0] < @rect2[0] + @rect2[2] &&
        @rect1[0] + @rect1[2] >  @rect2[0] &&
        @rect1[1] < @rect2[1] +  @rect2[3] &&
        @rect1[3] + @rect1[1] > @rect2[1])
-       @vie -=1
+        @vie -=1
     end
     if @vie==3
       @coeur.draw(0,0,0)
@@ -62,11 +63,8 @@ class Window < Gosu::Window
     elsif @vie ==2
       @coeur.draw(0,0,0)
       @coeur.draw(50,0,0)
-      @coeurvide.draw(100,0,0)
     elsif @vie ==1
       @coeur.draw(0,0,0)
-      @coeurvide.draw(50,0,0)
-      @coeurvide.draw(100,0,0)
     elsif @vie ==0
       close
     end
@@ -78,7 +76,6 @@ class Window < Gosu::Window
 
   end
   def button_down(id)
-
     case id
     when Gosu::KB_SPACE
       @player.try_to_jump
